929edd970bb18df42dcdf54179185978852bcd54
uber <uber@example.com>
2018-09-09 09:17:46 -0400

    added gt joystick

---
+++ src/bin/gt_config.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ltwheelconf  --wheel DFP --range=900 --nativemode --altautocenter=30 --rampspeed=1 --device /dev/input/event6
+
+
+# https://steamcommunity.com/sharedfiles/filedetails/?id=142372419
